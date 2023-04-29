554c2f0ba822dc90faf2d9055412607a57370b3c
fazalshahzad <shancompany322@gmail.com>
2023-04-29 14:27:40 +0500

    c

--- src/app/shared-service/product-api/product-api.service.ts
+++ src/app/shared-service/product-api/product-api.service.ts
@@ -16,16 +16,16 @@ export class ProductApiService {
     return this.http.get("http://localhost:8686/ProductManagement/GetProductData")
   }
   getProductWithId(id:any){
-return  this.http.get(`https://blog-backend-kohl.vercel.app//ProductManagement/getDocumentById/${id}`)  // 1st step create first slug and second slug then go to server.js commit section 👍👍👍 // after creating this slug getDocumentById/ then give id from here
+return  this.http.get(`http://localhost:8686//ProductManagement/getDocumentById/${id}`)  // 1st step create first slug and second slug then go to server.js commit section 👍👍👍 // after creating this slug getDocumentById/ then give id from here
   }
   deleteProductWithId(id:any){           //soft delete
-return  this.http.delete(`https://blog-backend-kohl.vercel.app//ProductManagement/DeleteProductById/${id}`)  
+return  this.http.delete(`http://localhost:8686//ProductManagement/DeleteProductById/${id}`)  
   }
   hardDeleteProductById(_id:any){
-  return this.http.delete(`https://blog-backend-kohl.vercel.app//ProductManagement/hardDeleteProductById/${_id}`)
+  return this.http.delete(`http://localhost:8686/ProductManagement/hardDeleteProductById/${_id}`)
   }
   upDateProductById(payLoad:any){ 
-return  this.http.post(`https://blog-backend-kohl.vercel.app//ProductManagement/updateProductById/`,payLoad)
+return  this.http.post(`http://localhost:8686/ProductManagement/updateProductById/`,payLoad)
   }
 //   getProductByCompanyName(companyName:any){
 // return this.http.get(`https://blog-backend-kohl.vercel.app//ProductManagement`)
